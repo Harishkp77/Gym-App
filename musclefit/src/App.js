afc30b0515ab107e9cac94bbd24410d72215ca07
Harishkp77 <shaarini_harish@outlook.com>
2023-07-29 23:29:23 +0530

    first commit

---
+++ musclefit/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Program from "./components/Program";
+import Membership from "./components/Membership";
+import Newsletter from "./components/Newsletter";
+import Footer from "./components/Footer";
+import ScrollUp from "./components/ScrollUp";
+import Loginpage from "./components/Loginpage";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Program />
+      <Membership />
+      <Newsletter />
+      <Loginpage />
+      <Footer />
+      <ScrollUp />
+    </>
+  );
+}
+
+export default App;
